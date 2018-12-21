1eee124e21215b9b291130718a5f35f67e8c83ce
Adam <spitzu@gmail.com>
2018-12-21 10:47:08 -0700

    finished Objective 4 Lab 3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner6;
+
+public class Objective4Lab3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int userNum;
+
+        System.out.println("Please enter a number: ");
+        userNum = scanner.nextInt();
+
+        if (userNum == 0)
+            System.out.println(userNum + " equals 0");
+        else if (userNum < 0)
+            System.out.println(userNum + " is less than 0");
+        else
+            System.out.println(userNum + " is greater than 0");
+
+        scanner.close();
+    }
+}
