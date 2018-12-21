4018688a1929632ed682eedfc6b51c41e8c7e821
Adam <spitzu@gmail.com>
2018-12-20 20:30:34 -0700

    finished Objective 1 Lab 3

--- Labs/Objective1Lab3.java
+++ Labs/Objective1Lab3.java
@@ -1,5 +1,5 @@
 public class Objective1Lab3{
     public static void main(String[] args){
-        System.out.println("Name : Adam Tappy\nAge : 37\nState: Colorado");
+        System.out.println("Name : Adam Tappy\nAge : 37\nState : Colorado");
     }
 }
