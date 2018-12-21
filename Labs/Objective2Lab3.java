7f1755ed2c7b1ee8c20e7669adad1ba0cf73e5de
Adam <spitzu@gmail.com>
2018-12-20 20:54:18 -0700

    finished Objective 2 Lab 3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3{
+    public static void main(String[] args){
+        String food1, food2, food3;
+
+        food1 = "Pizza";
+        food2 = "Pasta";
+        food3 = "Crab";
+
+        System.out.print("My Favorite Foods Are:\n1. " + food1 +
+            "\n2. " + food2 + "\n3. " + food3);
+    }
+}
