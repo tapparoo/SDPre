e720ae06f96849ae504170e1d85ded8cb6c9c048
Adam Tappy <adamtappy@Brucebs-MacBook-Pro-2.local>
2018-12-21 16:48:20 -0700

    finished Objective 6 Lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,10 @@
+public class Objective6Lab4{
+  public static void main(String[] args){
+    int counter = 1, sum = 0;
+
+    while(counter <= 20){
+      sum = sum + counter++;
+    }
+    System.out.println(sum);
+  }
+}
