656fbcc6243a56aabd90a5224b266d2cb26b1d86
Adam <spitzu@gmail.com>
2018-12-20 20:50:15 -0700

    finished Objective 2 Lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2{
+    public static void main(String[] args){
+        String name = "Charles Peterson";
+        int age = 47;
+        String state = "Arkansas";
+
+        name = "Adam Tappy";
+        age = 37;
+        state = "Colorado";
+
+        System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+    }
+}
