9e2eb9abdc64b59f77d2344a6f9c6e0bf7a0f411
Adam <spitzu@gmail.com>
2018-12-20 21:05:52 -0700

    finished Objective 2 Lab 5

--- Labs/Objective2Lab5.java
+++ Labs/Objective2Lab5.java
@@ -2,7 +2,7 @@ public class Objective2Lab5{
     public static void main(String[] args){
         double side1 = 10.0;
         double side2 = 8.0;
-        double hypotenuse = Math.sqrt((side1*side1) + (side2*side2));
+        double hypotenuse = Math.sqrt(side1*side1 + side2*side2);
 
         System.out.println("The hypotenuse of a triangle with sides " +
             side1 + " and " + side2 + " is " + hypotenuse);
